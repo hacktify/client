a6de2324b71d7f50494f121a00f33e429b1840f2
adityabpradana <adityabpradana@gmail.com>
2019-08-15 21:10:54 +0700

    add aplayer & social sharing

---
+++ src/components/aplayer.vue
@@ -0,0 +1,25 @@
+<template>
+    <aplayer autoplay
+        style="background-color: #222222; color: #ffffff;"
+      :music="{
+        title: 'Day 1',
+        artist: 'Honne',
+        theme: '#ff9900',
+        src: 'https://storage.googleapis.com/hacktify/1565875037204HONNE%20-%20Day%201%20%E2%97%91-64.mp3',
+        pic: 'https://moeplayer.b0.upaiyun.com/aplayer/secretbase.jpg'
+      }"
+    ></aplayer>
+</template>
+
+<script>
+import Aplayer from 'vue-aplayer'
+export default {
+    components: {
+        Aplayer
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/social-sharing.vue
@@ -0,0 +1,30 @@
+<template>
+   <social-sharing url="https://www.google.com"
+                      title="Hacktify: Music on Your Hand!"
+                      description="Hacktify is the best way to enjoy the music"
+                      hashtags="hacktify,vue,components"
+                      twitter-user="hacktify"
+                      inline-template>
+  <div>
+      <network network="facebook" class="btn btn-sm px-3" style="background-color: #3b5998; color: white;">
+        <i class="fa fa-facebook"></i>
+      </network>
+      <network network="twitter" class="btn btn-sm px-3 mx-2" style="background-color: #00acee; color: white;">
+        <i class="fa fa-twitter"></i>
+      </network>
+      <network network="whatsapp" class="btn btn-sm px-3" style="background-color: #25d366; color: white;">
+        <i class="fa fa-whatsapp"></i>
+      </network>
+  </div>
+</social-sharing>
+</template>
+
+<script>
+export default {
+    props: ['linkToMusic']
+}
+</script>
+
+<style>
+
+</style>
