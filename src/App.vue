5fc8799c475dabfca3417287dbe1e6b02100c8fe
justarya <just1arya@gmail.com>
2019-08-16 10:34:40 +0700

    Add Title to upload

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
-    <main-page></main-page>
+    <b-container>
+      <!-- <b-row class="justify-center">
+        <Login></Login>
+        <Register></Register>
+      </b-row>
+    </b-container> -->
+    <main-page :songs="songs" ></main-page>
     <upload></upload>
-    <song-page></song-page>
-    <aplayer></aplayer>
+    <!-- <song-page></song-page> -->
+    <aplayer class="shadow" :song="song"></aplayer>
   </div>
 </template>
 
 <script>
+import ax from '../config/axios'
+import Login from './components/Login'
+import Register from './components/Register'
 import MainPage from './components/main-page'
 import SongPage from './components/song-page'
 import Upload from './components/upload'
@@ -16,10 +25,31 @@ export default {
   data() {
     return {
       message: 'Hello world',
+      song: {
+        title: 'Day 1',
+        artist: 'Honne',
+        src: 'https://storage.googleapis.com/hacktify/1565875037204HONNE%20-%20Day%201%20%E2%97%91-64.mp3',
+        pic: 'https://via.placeholder.com/100/ff9900/ff9900'
+      },
+      songs: []
     };
   },
   components: {
-    MainPage, Upload, SongPage, Aplayer
+    MainPage,
+    Upload,
+    SongPage,
+    Aplayer,
+    Login,
+    Register
+  },
+  created() {
+    console.log('Created')
+    ax.get('/musics/allMusic')
+      .then(({data}) => {
+        console.log(data)
+        this.songs = data
+      })
+      .catch(console.log)
   }
 };
 </script>
---
+++ src/components/Login.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="col-4">
+        <div class="full-center">
+            <div id="login">
+                <h1 class="standout text-center">Log In</h1>
+                <form>
+                    <input class="form-control mt-4 shadow-sm" type="email" placeholder="Email">
+                    <input class="form-control mt-3 shadow-sm" type="password" placeholder="Password">
+                    <input class="form-control mt-3 btn-primary" type="submit" value="Log In">
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import ax from '../../config/axios'
+import GoogleSignInButton from 'vue-google-signin-button-directive'
+export default {
+    directives: {
+        GoogleSignInButton
+    },
+    data() {
+        return {
+            clientId: '973604254627-ca6u1b9pjmibaclhq74cfn303jip6cb6.apps.googleusercontent.com'
+        }
+    },
+    methods: {
+        OnGoogleAuthSuccess (idToken) {
+            ax({
+                url: '/users/signin/google',
+                method: 'post',
+                data: {
+                    id_token: idToken
+                }
+            })
+            .then(({ data })=> {
+                localStorage.setItem('token', data.access_token)
+                localStorage.setItem('username', data.username)
+                this.$emit('login:done')
+            })
+
+
+        },
+        OnGoogleAuthFail (error) {
+            console.log(error)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
